5f34e7cd003f5133fca86e39825567df4a149912
Parker Gabel <parker.d.gabel@gmail.com>
2025-04-17 16:14:32 -0700

    Changed prompt and added output

--- env.zsh
+++ env.zsh
@@ -1,7 +1,6 @@
   #################################################
 ##           Zsh Environment Variables             ##
-##################################################
 export EDITOR="nvim"
 export ZDOTDIR="$HOME"
 export PATH="$HOME/bin:/usr/local/bin:$PATH"
-
+ENABLE_CORRECTION="true"
