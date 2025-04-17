5b4237d09896de8b9d9c5ed2e6f8b48abd4fe2b2
Parker Gabel <parker.d.gabel@gmail.com>
2025-04-17 16:29:25 -0700

    Changed prompt and added output

--- zsh-config.plugin.zsh
+++ zsh-config.plugin.zsh
@@ -1,8 +1,10 @@
-source env.zsh
-source setup.zsh
-source plugins.zsh
-source options.zsh
-source functions.zsh
-source widgits.zsh
-source prompt.zsh
-source completions.zsh
+# Determine the directory of the current script
+script_dir="${0:A:h}"
+
+source "${script_dir}/env.zsh"
+source "${script_dir}/setup.zsh"
+source "${script_dir}/plugins.zsh"
+source "${script_dir}/options.zsh"
+source "${script_dir}/functions.zsh"
+source "${script_dir}/widgets.zsh"
+source "${script_dir}/prompt.zsh"
