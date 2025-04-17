76b182e487de98dc6685507f8ac48b33d033534b
Parker Gabel <parker.d.gabel@gmail.com>
2025-04-17 16:26:49 -0700

    Changed prompt and added output

--- zsh-config.plugin.zsh
+++ zsh-config.plugin.zsh
@@ -1,8 +1,8 @@
-source ./env.zsh
-source ./setup.zsh
-source ./plugins.zsh
-source ./options.zsh
-source ./functions.zsh
-source ./widgits.zsh
-source ./prompt.zsh
-source ./completions.zsh
+source env.zsh
+source setup.zsh
+source plugins.zsh
+source options.zsh
+source functions.zsh
+source widgits.zsh
+source prompt.zsh
+source completions.zsh
