6915021e1aea03b238bcd3a87142ae1464d4456b
Parker Gabel <parker.d.gabel@gmail.com>
2025-04-18 12:56:31 -0700

    Changed prompt and added output

--- plugins.zsh
+++ plugins.zsh
@@ -10,10 +10,11 @@ zinit light zsh-users/zsh-autosuggestions
 # Load zsh-autocomplete to provide advanced, context-based completions.
 zinit light marlonrichert/zsh-autocomplete
 zinit light junegunn/fzf
-zinit ice depth=1
+# zinit ice depth=1
 zinit light parkerdgabel/zsh-dnote
 zinit light parkerdgabel/zsh-config
 
-# Load last
+autoload -Uz compinit && compinit
 zinit light zdharma-continuum/fast-syntax-highlighting
+# Load last
 
