7a0c01a24dc41fee0d250698daffc4ec62f2c7d7
Parker Gabel <parker.d.gabel@gmail.com>
2025-04-18 12:08:23 -0700

    Changed prompt and added output

--- plugins.zsh
+++ plugins.zsh
@@ -6,7 +6,6 @@
 zinit light zsh-users/zsh-autosuggestions
 
 # Load faster-syntax-highlighting for real-time syntax coloring.
-# zinit light zdharma-continuum/fast-syntax-highlighting
 
 # Load zsh-autocomplete to provide advanced, context-based completions.
 zinit light marlonrichert/zsh-autocomplete
@@ -14,4 +13,7 @@ zinit light junegunn/fzf
 zinit ice depth=1
 zinit light parkerdgabel/zsh-dnote
 zinit light parkerdgabel/zsh-config
+
+# Load last
+zinit light zdharma-continuum/fast-syntax-highlighting
 
